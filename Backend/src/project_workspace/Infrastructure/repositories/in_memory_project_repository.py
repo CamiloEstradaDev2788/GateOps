3e5a70e6f22737bb2f48e6bea950baf3aabf131c
Soofocles <anasfajardol@gmail.com>
2026-02-18 16:13:53 -0500

    Adapter for memory

---
+++ Backend/src/project_workspace/Infrastructure/repositories/in_memory_project_repository.py
@@ -0,0 +1,21 @@
+from typing import List
+from uuid import UUID
+from project_workspace.domain.entities.project import Project
+from project_workspace.domain.ports.project_repository_port import ProjectRepositoryPort
+
+class InMemoryProjectRepository(ProjectRepositoryPort):
+
+    def __inti__(self):
+        self._projects: List[Project] = []
+
+    def save(self, project: Project) -> None:
+        self._projects.append(project)
+
+    def get_all(self) -> List[Project]:
+        return self._projects
+    
+    def get_by_id(self, project_id: UUID) -> Project | None:
+        for project in self._projects:
+            if project.id == project_id:
+                return project
+        return None
